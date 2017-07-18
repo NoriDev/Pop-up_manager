ff4b329da6156e24822f1918b33fa13e9184dfe7
NoriDev <m1nthing2322@gmail.com>
2017-07-18 14:38:43 +0900

    Initial commit

---
+++ mobile/src/main/java/i2c/noridev/m1nthing2322/popup_manager/main/MainActivity.java
@@ -0,0 +1,15 @@
+package i2c.noridev.m1nthing2322.popup_manager.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import i2c.noridev.m1nthing2322.popup_manager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
